b0138b92e6e6d5050eafbcbeed5b466a4bc0749f
Richard Griffiths <Soulfiremage@hotmail.co.uk>
2015-11-16 09:54:16 +0000

    Speed coding basics exercise: results at 45mins

---
+++ TimedTest/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace TimedTest
+    {
+    /// <summary>
+    /// This is a timed test with the following spec:
+    /// The app must give 3 boxes that allow you to produce a list of countries, with cities and people in each city
+    /// Predefined data generation does not count in the development time!
+    /// 
+    /// </summary>
+    public partial class MainWindow : Window
+        {
+        public List<Person> people = new List<Person>();
+        public List<country> Countries = new List<country>();
+
+        public MainWindow()
+            {
+            InitializeComponent();
+            Dictionary<string, List<string>> citiesData = new Dictionary<string, List<string>>() { { "France", new List<String>() { "Paris", "Lyon" } },
+                                                                                                   { "England", new List<string>() {"Birmingham", "Bristol" } },
+                                                                                                   {"Spain", new List<string>() {"Madrid" } },
+                                                                                                    {"Germany", new List<string>() {"Dusseldorf","Berlin" } } };
+
+            foreach (string c in new List<string>() { "France", "England", "Spain", "Germany" })
+                {
+                Countries.Add(new country(c, citiesData[c].Select(x => new City(x)).ToList())); //NO Checking of keys is happening here - I know! and yes, I cheated with the lambda :P
+                }
+
+
+            foreach (country c in Countries)
+                {
+                Results.Text += $"{c.Name}\n"; //could have abstracted even this :P
+                foreach (City city in c.Cities)
+                    {
+                    Results.Text += $"\t\t{city.Name}\n";
+                    }
+                }
+            }
+        }
+    }
+
+
+     
---
+++ TimedTest/Named.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimedTest
+    {
+    public abstract class Named
+        {
+        private string _name; //check this access, likely to need to be internal!
+        public string Name
+            {
+            get { return _name; }
+            set { _name = _name ?? value; }
+            }
+
+        public Named(string name) {
+            _name = name;
+            }
+        }
+
+    public class country : Named
+        {
+        private List<City> _cities;
+        public List<City> Cities { get { return _cities; }
+            set { _cities = value ?? new List<City>(); }  //must double check this idiom! 
+            }
+        public country(string name) : base(name) { }
+        public country(string name, List<City> cities) :base(name)
+            {
+            _cities = cities;
+            }
+
+        }
+
+    public class City : Named
+        {
+        private List<Person> _people;
+        public List<Person> People { get { return _people; } set { _people = value ?? new List<Person>(); } }
+
+        public City(string name) : base(name) { }
+        public City(string name, List<Person> people) :base(name)
+            {
+            _people = people;
+            }
+        }
+
+
+    public class Person : Named
+        {
+        private string _firstname;
+        public string FirstName { get { return _firstname; } set { _firstname = value ?? _firstname; } }
+        private string _surname;
+        public string SurName { get { return SurName; } set { _surname = value ?? _surname; } } 
+        public Person(string name) :base(name)  {
+            _firstname = name.Split(' ')[0];
+            _surname = name.Split(' ')[1];
+            }
+        }
+    }
